a4a59261511f0893c819c1eadf1ec4730642cec2
SalHe Li <SalHe@qq.com>
2022-05-13 08:56:20 +0800

    chore:

--- api/token/token.go
+++ api/token/token.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"strconv"
+	"sync"
 
 	"github.com/golang-jwt/jwt"
 	"github.com/se2022-qiaqia/course-system/config"
@@ -14,6 +15,7 @@ import (
 var (
 	tokenJwt = make(map[string]string)
 	jwtToken = make(map[string]string)
+	mux      = &sync.Mutex{}
 
 	signMethod = jwt.SigningMethodHS512
 )
@@ -41,10 +43,25 @@ func NewJwt(user *dao.User) string {
 	return signingString
 }
 
+func saveToken(token string, jwt string) {
+	mux.Lock()
+	defer mux.Unlock()
+
+	tokenJwt[token] = jwt
+}
+
+func getJwt(token string) (string, bool) {
+	mux.Lock()
+	defer mux.Unlock()
+
+	jwt2, ok := tokenJwt[token]
+	return jwt2, ok
+}
+
 func NewToken(user *dao.User) string {
 	newJwt := NewJwt(user)
 	token := FromJwt(newJwt)
-	tokenJwt[token] = newJwt
+	saveToken(token, newJwt)
 	return token
 }
 
@@ -54,12 +71,26 @@ func md5Hash(content string) string {
 	return fmt.Sprintf("%x", token)
 }
 
+func saveJwt(jwt string, token string) {
+	mux.Lock()
+	defer mux.Unlock()
+	jwtToken[jwt] = token
+}
+
+func getToken(jwt string) (string, bool) {
+	mux.Lock()
+	defer mux.Unlock()
+
+	s, b := jwtToken[jwt]
+	return s, b
+}
+
 func FromJwt(jwt string) string {
-	if s, found := jwtToken[jwt]; found {
+	if s, found := getToken(jwt); found {
 		return s
 	} else {
 		token := md5Hash(jwt)
-		jwtToken[jwt] = token
+		saveJwt(jwt, token)
 		return token
 	}
 }
@@ -78,7 +109,8 @@ func ClaimsFromJwt(jwtString string) *JwtClaims {
 }
 
 func ToClaims(token string) *JwtClaims {
-	return ClaimsFromJwt(tokenJwt[token])
+	jwt2, _ := getJwt(token)
+	return ClaimsFromJwt(jwt2)
 }
 
 func (c *JwtClaims) IsAdmin() bool {
